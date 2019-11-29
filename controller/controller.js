2b8ddb818e802b4212283858bc1b5a78d2ac3d17
OLIVIER AUBERGER <oliviera89@gmail.com>
2019-11-29 08:36:46 -0600

    request from eurosport.com is working. However it needs improvements. Handlebars are too inspired from scraper_news and will be changed

---
+++ controller/controller.js
@@ -0,0 +1,159 @@
+//The whole homework is strongly inspired by https://github.com/Dcoxmen/scraper_news
+
+
+var express = require("express");
+var router = express.Router();
+var path = require("path");
+
+var request = require("request");
+var cheerio = require("cheerio");
+
+var Note = require("../models/Note.js");
+var Article = require("../models/Article.js");
+
+router.get("/", function(req, res) {
+  res.redirect("/articles");
+});
+
+router.get("/getnews", function(req, res) {
+  request("https://www.eurosport.com/", function(error, response, html) {
+    var $ = cheerio.load(html);
+    var titlesArray = [];
+
+    $("div").each(function(i, element) {
+      var result = {};
+
+      result.title = $(this)
+        .children("a")
+        .text();
+      result.link = $(this)
+        .children("a")
+        .attr("href");
+
+      if (result.title !== "" && result.link !== "") {
+        if (titlesArray.indexOf(result.title) == -1) {
+          titlesArray.push(result.title);
+
+          Article.count({ title: result.title }, function(err, test) {
+            if (test === 0) {
+              var entry = new Article(result);
+
+              entry.save(function(err, doc) {
+                if (err) {
+                  console.log(err);
+                } else {
+                  console.log(doc);
+                }
+              });
+            }
+          });
+        } else {
+          console.log("Article already exists.");
+        }
+      } else {
+        console.log("Not saved to DB, missing data");
+      }
+    });
+    res.redirect("/");
+  });
+});
+router.get("/articles", function(req, res) {
+  Article.find()
+    .sort({ _id: -1 })
+    .exec(function(err, doc) {
+      if (err) {
+        console.log(err);
+      } else {
+        var artcl = { article: doc };
+        res.render("index", artcl);
+      }
+    });
+});
+
+router.get("/articles-json", function(req, res) {
+  Article.find({}, function(err, doc) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(doc);
+    }
+  });
+});
+
+router.get("/clearAll", function(req, res) {
+  Article.remove({}, function(err, doc) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("removed all articles");
+    }
+  });
+  res.redirect("/articles-json");
+});
+
+router.get("/readArticle/:id", function(req, res) {
+  var articleId = req.params.id;
+  var hbsObj = {
+    article: [],
+    body: []
+  };
+
+  Article.findOne({ _id: articleId })
+    .populate("note")
+    .exec(function(err, doc) {
+      if (err) {
+        console.log("Error: " + err);
+      } else {
+        hbsObj.article = doc;
+        var link = doc.link;
+        request(link, function(error, response, html) {
+          var $ = cheerio.load(html);
+
+          $(".l-col__main").each(function(i, element) {
+            hbsObj.body = $(this)
+              .children(".c-entry-content")
+              .children("p")
+              .text();
+
+            res.render("article", hbsObj);
+            return false;
+          });
+        });
+      }
+    });
+});
+router.post("/note/:id", function(req, res) {
+  var user = req.body.name;
+  var content = req.body.comment;
+  var articleId = req.params.id;
+
+  var noteObj = {
+    name: user,
+    body: content
+  };
+
+  var newNote = new Note (noteObj);
+
+  newNote.save(function(err, doc) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(doc._id);
+      console.log(articleId);
+
+      Article.findOneAndUpdate(
+        { _id: req.params.id },
+        { $push: { note: doc._id } },
+        { new: true }
+      ).exec(function(err, doc) {
+        if (err) {
+          console.log(err);
+        } else {
+          res.redirect("/readArticle/" + articleId);
+        }
+      });
+    }
+  });
+});
+
+module.exports = router;
