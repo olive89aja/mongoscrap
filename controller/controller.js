bcc9cb3409f252a90ef39c632fed1124a84f644a
OLIVIER AUBERGER <oliviera89@gmail.com>
2019-11-30 22:24:20 -0600

    Trying to refine news 6

--- controller/controller.js
+++ controller/controller.js
@@ -25,7 +25,7 @@ router.get("/getnews", function(req, res) {
     var $ = cheerio.load(html);
     var titlesArray = [];
 
-    $(".storylist-latest").each(function(i, element) {
+    $(".storylist-container-latest").each(function(i, element) {
       var result = {};
 
       result.title = $(this)
